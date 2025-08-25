eadabbe790007586a489ce83aadc248234eada26
estevao-ueek <estevao@ueek.digital>
2025-08-25 16:47:57 -0300

    Implementado teste de integração para edição de gêneros

--- ControleDeCinema.Testes.Integracao/ModuloGenero/RepositorioGeneroFilmeEmOrmTests.cs
+++ ControleDeCinema.Testes.Integracao/ModuloGenero/RepositorioGeneroFilmeEmOrmTests.cs
@@ -21,4 +21,25 @@ public sealed class RepositorioGeneroFilmeEmOrmTests : TestFixture
 
         Assert.AreEqual(genero, generoEncontrado);
     }
+
+    [TestMethod]
+    public void Deve_Editar_GeneroFilme_Corretamente()
+    {
+        // Arrange
+        var genero = new GeneroFilme("Comédia");
+        repositorioGeneroFilme?.Cadastrar(genero);
+        dbContext?.SaveChanges();
+
+        var generoEditado = new GeneroFilme("Comédia Romântica");
+
+        // Act
+        var conseguiuEditar = repositorioGeneroFilme?.Editar(genero.Id, generoEditado);
+        dbContext?.SaveChanges();
+
+        // Assert
+        var generoEncontrado = repositorioGeneroFilme?.SelecionarRegistroPorId(genero.Id);
+
+        Assert.IsTrue(conseguiuEditar);
+        Assert.AreEqual(genero, generoEncontrado);
+    }
 }
