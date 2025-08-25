4718588b7b986655a4eed5a300f50f9a675a565c
estevao-ueek <estevao@ueek.digital>
2025-08-25 16:30:35 -0300

    Implementado configuração inicial dos testes com TestFixture

---
+++ ControleDeCinema.Testes.Integracao/Compartilhado/TestFixture.cs
@@ -0,0 +1,78 @@
+using ControleDeCinema.Dominio.ModuloGeneroFilme;
+using ControleDeCinema.Infraestrutura.Orm.Compartilhado;
+using ControleDeCinema.Infraestrutura.Orm.ModuloGeneroFilme;
+using DotNet.Testcontainers.Containers;
+using FizzWare.NBuilder;
+using Testcontainers.PostgreSql;
+
+namespace ControleDeCinema.Testes.Integracao;
+
+[TestClass]
+public abstract class TestFixture
+{
+    protected ControleDeCinemaDbContext? dbContext;
+
+    protected RepositorioGeneroFilmeEmOrm? repositorioGeneroFilme;
+
+    private static IDatabaseContainer? container;
+
+    [AssemblyInitialize]
+    public static async Task Setup(TestContext _)
+    {
+        container = new PostgreSqlBuilder()
+            .WithImage("postgres:16")
+            .WithName("controle-de-cinema-testdb")
+            .WithDatabase("ControleDeCinemaTestDb")
+            .WithUsername("postgres")
+            .WithPassword("root")
+            .WithCleanUp(true)
+            .Build();
+
+        await InicializarBancoDadosAsync(container);
+    }
+
+    [AssemblyCleanup]
+    public static async Task Teardown()
+    {
+        await EncerrarBancoDadosAsync();
+    }
+
+    [TestInitialize]
+    public void ConfigurarTestes()
+    {
+        if (container is null)
+            throw new ArgumentNullException("O banco de dados não foi inicializado.");
+
+        dbContext = ControleDeCinemaDbContextFactory.CriarDbContext(container.GetConnectionString());
+
+        ConfigurarTabelas(dbContext);
+
+        repositorioGeneroFilme = new RepositorioGeneroFilmeEmOrm(dbContext);
+
+        BuilderSetup.SetCreatePersistenceMethod<GeneroFilme>(repositorioGeneroFilme.Cadastrar);
+        BuilderSetup.SetCreatePersistenceMethod<IList<GeneroFilme>>(repositorioGeneroFilme.CadastrarEntidades);
+    }
+
+    private static void ConfigurarTabelas(ControleDeCinemaDbContext dbContext)
+    {
+        dbContext.Database.EnsureCreated();
+
+        dbContext.GenerosFilme.RemoveRange(dbContext.GenerosFilme);
+
+        dbContext.SaveChanges();
+    }
+
+    private static async Task InicializarBancoDadosAsync(IDatabaseContainer container)
+    {
+        await container.StartAsync();
+    }
+
+    private static async Task EncerrarBancoDadosAsync()
+    {
+        if (container is null)
+            throw new ArgumentNullException("O Banco de dados não foi inicializado.");
+
+        await container.StopAsync();
+        await container.DisposeAsync();
+    }
+}
---
+++ ControleDeCinema.Testes.Integracao/ModuloGenero/RepositorioGeneroFilmeEmOrmTests.cs
@@ -0,0 +1,11 @@
+namespace ControleDeCinema.Testes.Integracao;
+
+[TestClass]
+[TestCategory("Testes de Integração de Gênero de Filme")]
+public sealed class RepositorioGeneroFilmeEmOrmTests : TextFixture
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
