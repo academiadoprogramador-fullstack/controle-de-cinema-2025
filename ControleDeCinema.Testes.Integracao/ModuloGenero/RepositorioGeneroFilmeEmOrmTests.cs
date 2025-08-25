465ca75854c3170d2d3bd3729f72130a8bf8f209
estevao-ueek <estevao@ueek.digital>
2025-08-25 16:38:15 -0300

    Implementado teste de integração de cadastro de gêneros

--- ControleDeCinema.Testes.Integracao/ModuloGenero/RepositorioGeneroFilmeEmOrmTests.cs
+++ ControleDeCinema.Testes.Integracao/ModuloGenero/RepositorioGeneroFilmeEmOrmTests.cs
@@ -1,11 +1,24 @@
+using ControleDeCinema.Dominio.ModuloGeneroFilme;
+
 namespace ControleDeCinema.Testes.Integracao;
 
 [TestClass]
 [TestCategory("Testes de Integração de Gênero de Filme")]
-public sealed class RepositorioGeneroFilmeEmOrmTests : TextFixture
+public sealed class RepositorioGeneroFilmeEmOrmTests : TestFixture
 {
     [TestMethod]
-    public void TestMethod1()
+    public void Deve_Cadastrar_GeneroFilme_Corretamente()
     {
+        // Arrange
+        var genero = new GeneroFilme("Ação");
+
+        // Act
+        repositorioGeneroFilme?.Cadastrar(genero);
+        dbContext?.SaveChanges();
+
+        // Assert
+        var generoEncontrado = repositorioGeneroFilme?.SelecionarRegistroPorId(genero.Id);
+
+        Assert.AreEqual(genero, generoEncontrado);
     }
 }
